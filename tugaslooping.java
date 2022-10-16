ff8bc2b4a91cfc7d46897e81e093307ab82bdf95
Torui Natsuya <113415824+AmandaMahardika@users.noreply.github.com>
2022-10-16 13:55:04 +0700

    Add files via upload
    tugas looping bersarang kelompok 12 dengan metode for

---
+++ tugaslooping.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasperulangan;
+
+/**
+ *
+ * @author User
+ */
+public class tugaslooping {
+    public static void main(String[] args) {
+        for (int a=1; a<=4; a++) {
+            for (int b=5; b>1; b--) {
+                    for (int c=5; c<=5; c++) {
+                }
+                System.out.print("*");
+            }
+                System.out.println("0");
+                }
+                System.out.println("00000");
+    }
+}
